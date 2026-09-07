5dc8bedc9df7e4b04f6ae94b80313be040af6546
Naresh-ado <nrshsenthil@gmail.com>
2026-09-07 23:47:47 +0530

    Time: 4 ms (96.08%), Space: 47.6 MB (60.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,6 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) 
     {
+
+        int[] lastseen = new int[128];
+        int left = 0;
+        int maxsize = 0;
+        for(int right = 0;right<s.length();right++)
+        {
+            char ch = s.charAt(right);
+            left = Math.max(left,lastseen[ch]);
+            maxsize = Math.max(maxsize,right - left + 1);
+
+            lastseen[ch] = right + 1;
+        }
+        return maxsize;
+
+
+
+
+
+
+
+
+
+        /* 
+
+        Intead of when seen a duplicate removing the character one by one why can't if i known the index of that we can directly move to the next character after this can be done using HashMap or freq[] array since String consists if only char Array
+
         int maxsize = 0;
         int left = 0;
         HashMap<Character,Integer> window = new HashMap<>();
@@ -24,7 +50,7 @@ class Solution {
             }
 
         }
-        return maxsize;
+        return maxsize; */
         
     }
 }
