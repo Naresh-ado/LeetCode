b1570b9fd8a9ba2adf29f5570047e3776c3fc060
Naresh S <nrshsenthil@gmail.com>
2026-09-15 19:53:45 +0530

    Time: 3 ms (95.02%), Space: 46.2 MB (94.16%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,43 +1,44 @@
 class Solution {
-    public String convert(String s, int k) 
-    {
-        if(s.length() <= k || k==1)return s;
+    public String convert(String s, int k) {
+
+        if (k == 1 || s.length() <= k)
+            return s;
+
         StringBuilder sb = new StringBuilder();
-        int toprow = 2 * (k - 1);
-        int row = 0;
-        while(row < s.length())
-        {
-            sb.append(s.charAt(row));
-            row += toprow;
 
-        }
-        for(int r=1;r<k-1;r++)
-        {
+        for (int r = 0; r < k; r++) {
+
             int first = 2 * (k - 1 - r);
-            int second  = 2 * r;
-            int ptr = r;
-            sb.append(s.charAt(ptr));
-            while(ptr < s.length())
-            {
-                ptr+=first;
-                if(ptr < s.length()){
-                    sb.append(s.charAt(ptr));
-                }
-                ptr+=second;
-                if(ptr < s.length()){
-                    sb.append(s.charAt(ptr));
-                }
-                
-            }
+            int second = 2 * r;
 
+            int ptr = r;
+
+            sb.append(s.charAt(ptr));
+
+            while (true) {
+
+                // First jump
+                if (first != 0) {
+                    ptr += first;
+
+                    if (ptr >= s.length())
+                        break;
+
+                    sb.append(s.charAt(ptr));
+                }
+
+                // Second jump
+                if (second != 0) {
+                    ptr += second;
+
+                    if (ptr >= s.length())
+                        break;
+
+                    sb.append(s.charAt(ptr));
+                }
+            }
         }
-        row = k-1;
-        while(row < s.length())
-        {
-            sb.append(s.charAt(row));
-            row += toprow;
-        }
+
         return sb.toString();
-        
     }
 }
