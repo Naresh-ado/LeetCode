45a20c3a696e7b7af3624e59f79e51eb79daa217
Naresh S <nrshsenthil@gmail.com>
2026-09-15 23:35:20 +0530

    Time: 1794 ms (5.07%), Space: 50.3 MB (15.42%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,73 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+        List<List<String>> list = new ArrayList<>();
+        int[] freq = new int[strs.length];
+        
+        //Edge case Empty String handling
+        int efound = 0;
+        List<String> emp = new ArrayList<>();
+        for(int i=0;i<strs.length;i++)
+        {
+            if(strs[i].length() == 0)
+            {
+                emp.add(strs[i]);
+                freq[i] = 1;
+                efound = 1;
+            }
+        }
+        if(efound == 1)list.add(emp);
+        
+        for(int i=0;i<strs.length;i++)
+        {
+            List<String> l = new ArrayList<>();
+            HashMap<Character,Integer> map = new HashMap<>();
+            if(freq[i] == 0)
+            {
+                l.add(strs[i]);
+                freq[i] = 1;
+                for(char ch : strs[i].toCharArray())
+                {
+                    map.put(ch,map.getOrDefault(ch,0) + 1);
+                }
+                for(int j=i+1;j<strs.length;j++)
+                {
+                    if(freq[j] == 0)
+                    {
+                        String sd = strs[j];
+
+                        if(strs[i].length() != sd.length())
+                            continue;
+
+                        HashMap<Character,Integer> m2 = new HashMap<>();
+
+                        for(char ch : sd.toCharArray())
+                        {
+                            m2.put(ch, m2.getOrDefault(ch,0) + 1);
+                        }
+
+                        int found = 0;
+
+                        for(char ch : strs[i].toCharArray())
+                        {
+                            if(!map.get(ch).equals(m2.get(ch)))
+                            {
+                                found = 1;
+                                break;
+                            }
+                        }
+
+                        if(found == 0)
+                        {
+                            l.add(sd);
+                            freq[j] = 1;
+                        }
+                    }
+                }
+                list.add(l);
+            }
+            
+        }
+        return list;
+    }
+}
