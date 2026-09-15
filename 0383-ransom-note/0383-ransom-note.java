c3905d85ac4c92fc08243a9ecb0304ea47e4f7b3
Naresh S <nrshsenthil@gmail.com>
2026-09-15 07:59:09 +0530

    Time: 1 ms (99.95%), Space: 46.5 MB (57.81%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,7 +1,26 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) 
     {
-        char[] freq1 = new char[26];
+
+        //USING ONLY 1 ARRAY
+        int[] freq = new int[26];
+        for(char ch : magazine.toCharArray())
+        {
+            freq[ch - 'a']++;
+        }
+
+        for(char ch : ransomNote.toCharArray())
+        {
+            freq[ch - 'a']--;
+            if(freq[ch-'a'] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+        /*
+        char[] freq1 = new char[26]; here actually you need to use int 
         char[] freq2 = new char[26];
         for(char ch : ransomNote.toCharArray())
         {
@@ -20,6 +39,6 @@ class Solution {
             }
         }
         return true;
-        
+        */
     }
 }
