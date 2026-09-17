dd7e0613bc2c8a3bf7cef87610fc9b330cd62abb
Naresh S <nrshsenthil@gmail.com>
2026-09-17 17:13:34 +0530

    Time: 1 ms (99.89%), Space: 63.3 MB (9.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) 
     {
+        //Using Boyre-Moore voting algorithm
+        int count = 0;
+        int candidate = 0;
+        for(int x : nums)
+        {
+            if(count == 0)
+            {
+                candidate = x;
+            }
+
+            if(candidate == x)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return candidate;
+        /*
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int x : nums)
         {
@@ -15,6 +36,6 @@ class Solution {
             }
         }
         return n;
-        
+        */
     }
 }
